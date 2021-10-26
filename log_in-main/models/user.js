4e53b95cebd2ca991f79c85c32dc623687af21c6
pablowaller <juanpablowaller76@gmal.com>
2021-10-25 21:00:44 -0300

    commit

--- log_in-main/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("Users", {
-  username: String,
-  password: String,
-  facebook_id: String,
-  email: String,
-  photo: String,
-});
